b348fe9b7fefc0bce250ee928ffde04d28c15e56
BK <bekr@dell.home.tld>
2015-02-21 18:13:33 +0100

    Initial commit

---
+++ modules/hieradata/manifests/config.pp
@@ -0,0 +1,38 @@
+##
+## Manage Hiera data
+##
+class hieradata::config {
+
+    ## Install hieradata for puppet-server
+	if ( $::ipaddress in $::hieradata::params::puppet_server_ipaddress_list ) {
+    
+        include puppet_server
+    
+        ## Install hiera configuration
+        file { "/etc/puppet/hiera.yaml" :
+             ensure => present,
+             source => "puppet:///modules/hieradata/hiera.yaml",
+              owner => 'root',
+              group => 'root',
+            require => Class["puppet_server::install"],
+             notify => Class["puppet_server::service"],
+        }
+        
+        file {'/etc/hiera.yaml':
+            ensure => link,
+            target => '/etc/puppet/hiera.yaml',
+            require => File['/etc/puppet/hiera.yaml'],
+        }
+        
+        file { '/etc/puppet/hieradata':
+            ensure => directory,
+        }
+            
+        file { '/etc/puppet/hieradata/node':
+             ensure => directory,
+            require => File['/etc/puppet/hieradata'],
+        }
+    
+    }
+
+}
---
+++ modules/hieradata/manifests/params.pp
@@ -0,0 +1,8 @@
+##
+## Managed puppet-server
+##
+class hieradata::params {
+
+    $puppet_server_ipaddress_list = [ '192.168.0.222', '176.10.168.227' ]
+
+}
