2eebf0842880ea6210a7ff46a79ef6d81c03dfe5
BK <bekr@dell.home.tld>
2015-02-22 20:42:23 +0100

    Remove to reload bashrc

--- modules/root_bashrc/manifests/init.pp
+++ modules/root_bashrc/manifests/init.pp
@@ -4,7 +4,7 @@
 class root_bashrc {
 
     if ! ( $::operatingsystem == 'Debian' ) {
-        fail("FAIL: This module (root_bashrc) is only for Debian based distributions! Aborting...")
+        fail('FAIL: This module (root_bashrc) is only for Debian based distributions! Aborting...')
     }
 
     include root_bashrc::config
