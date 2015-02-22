83c1568c52c13970ebaf602d37fda2dcca393afb
BK <bekr@dell.home.tld>
2015-02-22 19:34:55 +0100

    Lint code

--- modules/hieradata/manifests/config.pp
+++ modules/hieradata/manifests/config.pp
@@ -14,13 +14,13 @@ class hieradata::config {
         include puppet_server
 
         ## Install hiera configuration
-        file { "/etc/puppet/hiera.yaml" :
+        file { '/etc/puppet/hiera.yaml' :
             ensure  => present,
-            source  => "puppet:///modules/hieradata/hiera.yaml",
+            source  => 'puppet:///modules/hieradata/hiera.yaml',
             owner   => 'root',
             group   => 'root',
-            require => Class["puppet_server::install"],
-            notify  => Class["puppet_server::service"],
+            require => Class['puppet_server::install'],
+            notify  => Class['puppet_server::service'],
         }
 
         file {'/etc/hiera.yaml':
