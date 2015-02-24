b1f961ef7022405e5f5aa0603a2a269ac1584fd8
BK <bekr@dell.home.tld>
2015-02-24 10:19:59 +0100

    Move puppet server data to one params

--- modules/hieradata/manifests/init.pp
+++ modules/hieradata/manifests/init.pp
@@ -4,7 +4,7 @@
 class hieradata {
 
     if ! ( $::operatingsystem == 'Debian' ) {
-        fail("FAIL: This module (hieradata) is only for Debian based distributions! Aborting...")
+        fail('FAIL: This module (hieradata) is only for Debian based distributions! Aborting...')
     }
 
     include hieradata::config
