a9bb566aa25c7ba423eaed8d2c04bfd05e3b5bde
BK <bekr@dell.home.tld>
2015-02-24 10:16:55 +0100

    Move puppet server data to one params

--- modules/profile/manifests/puppetize.pp
+++ modules/profile/manifests/puppetize.pp
@@ -1,22 +1,40 @@
 ##
-## Bootstrap puppet (Note: puppet-server IPs are duplicated in these three params)
+## Bootstrap puppet
 ##
 class profile::puppetize {
 
+    include profile::params
+    $puppet_server_ipaddress_local      = $::puppet_server::params::puppet_server_ipaddress_local
+    $puppet_server_ipaddress_public_176 = $::puppet_server::params::puppet_server_ipaddress_public_176
+
+    ## Is host in private network or in 176-public network
+    if $::ipaddress =~ /^192\./ {
+        $puppet_server_fqdn         = 'puppet.home.tld'
+        $puppet_server_ipaddress    = '192.168.0.222'
+    }
+    elsif $::ipaddress =~ /^176\./ {
+        $puppet_server_fqdn         = 'puppet.triatagroup.com'
+        $puppet_server_ipaddress    = '176.10.168.227'
+    }
+    else {
+        fail("FAIL: Host IPv4 ($::ipaddress) is not on any managed network!")
+    }
+
+    ## Install puppet_server configuration
+    if ( $::ipaddress == $puppet_server_ipaddress ) {
+        class { puppet_server::config :
+            server_fqdn => $puppet_server_fqdn,
+            server_ip   => $puppet_server_ipaddress,
+        }
+        include hieradata
+    }
 
     ## puppet_agent and git are always installed on all hosts!
-    
-    ## looks up where puppet_server hosts IP addresses are in  'puppet_agent::params'
-    include puppet_agent
+    class { puppet_agent::config :
+        server_fqdn => $puppet_server_fqdn,
+        server_ip   => $puppet_server_ipaddress,
+    }
+
     include git_client
-
-
-    ## Only installed on puppet_server!
-    
-    ## looks up assigned hosts IP addresses are in 'puppet_server::params'
-    include puppet_server
-    
-    ## looks up where puppet_server hosts IP addresses are in 'hieradata::params'
-    include hieradata
 
 }
