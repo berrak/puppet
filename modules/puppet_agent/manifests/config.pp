3174b5ae96a5e0b7998245e8584b1e4307c3f005
berrak <bkronmailbox-git@yahoo.se>
2015-03-06 22:33:15 +0100

    Change notice to info level

--- modules/puppet_agent/manifests/config.pp
+++ modules/puppet_agent/manifests/config.pp
@@ -22,7 +22,7 @@ class puppet_agent::config ( $server_fqdn_for_agent, $server_ip_for_agent ) {
         $puppet_server_fqdn         = $server_fqdn_for_agent
         $puppet_server_ipaddress    = $server_ip_for_agent
 
-        notify { "Puppet server IPs ${puppet_server_ipaddress} known to puppet_agent": loglevel => notice }
+        notify { "Puppet server IPs ${puppet_server_ipaddress} known to puppet_agent": loglevel => info }
         notify { "Puppet server FQDN ${puppet_server_fqdn} known to puppet_agent": loglevel => info }
 
         $myhostname = $::hostname
--- modules/puppet_agent/manifests/install.pp
+++ modules/puppet_agent/manifests/install.pp
@@ -2,34 +2,34 @@
 ## Manage Puppet clients
 ##
 class puppet_agent::install {
-  
-	package { [ 'puppet', 'facter' ] :
-			ensure => present,
-	 allow_virtual => true,
-	}
-		
-	## Utilities for root
+
+    package { [ 'puppet', 'facter' ] :
+        ensure        => present,
+        allow_virtual => true,
+    }
+
+    ## Utilities for root
     file { '/root/bin' :
         ensure => directory,
-         owner => 'root',
-         group => 'root',
-          mode => '0700',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0700',
     }
-	
-	file { '/root/bin/puppet.exec':
-		 source => 'puppet:///modules/puppet_agent/puppet.exec',
-		  owner => 'root',
-		  group => 'root',
-		   mode => '0700',
-		require => File['/root/bin'],
-	}	
-	
+
+    file { '/root/bin/puppet.exec':
+        source => 'puppet:///modules/puppet_agent/puppet.exec',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0700',
+        require => File['/root/bin'],
+    }
+
     file { '/root/bin/puppet.simulate':
-	     source => 'puppet:///modules/puppet_agent/puppet.simulate',
-	      owner => 'root',
-	      group => 'root',
-	       mode => '0700',
-		require => File['/root/bin'],		  
+        source  => 'puppet:///modules/puppet_agent/puppet.simulate',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0700',
+        require => File['/root/bin'],
     }
-	
+
 }
