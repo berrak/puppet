7cc405adde066f318697ea51c12b7eb85795158c
berrak <bkronmailbox-git@yahoo.se>
2015-03-07 09:06:38 +0100

    Change class to define

--- modules/puppet_agent/manifests/service.pp
+++ modules/puppet_agent/manifests/service.pp
@@ -3,12 +3,12 @@
 ##
 class puppet_agent::service {
 
-	## Jessie systemd defaults for agent is 'running' - disable daemon!
+    ## Jessie systemd defaults for agent is 'running' - disable daemon!
     service { 'puppet_agent':
-              name => 'puppet',
-            enable => false,
-            ensure => stopped,
-	       require => Class['puppet_agent::install'],
+        name    => 'puppet',
+        enable  => false,
+        ensure  => stopped,
+        require => Class['puppet_agent::install'],
     }
-	
+
 }
