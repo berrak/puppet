110d1ade5055589619274f7dd647df96dead0918
berrak <bkronmailbox-git@yahoo.se>
2015-03-07 08:24:06 +0100

    Add hosts mangement

--- modules/profile/manifests/puppetize.pp
+++ modules/profile/manifests/puppetize.pp
@@ -4,12 +4,18 @@
 class profile::puppetize {
 
     include git_client
-
-    ## Is host in private network or in 176-public network
+    ##
+    ## Install puppet_agent configuration
+    ##
+    # Is host in private network or in 176-public network
     if $::ipaddress =~ /^192\./ {
         class { puppet_agent::config :
             server_fqdn_for_agent => 'puppet.home.tld',
             server_ip_for_agent   => '192.168.0.222',
+        }
+        hosts::agent { "$::hostname" :
+            puppet_server_ip => '192.168.0.222',
+            network_domain   => 'home.tld',
         }
     }
     elsif $::ipaddress =~ /^176\./ {
@@ -17,18 +23,24 @@ class profile::puppetize {
             server_fqdn_for_agent => 'puppet.triatagroup.com',
             server_ip_for_agent   => '176.10.168.227',
         }
+        hosts::agent { "$::hostname" :
+            puppet_server_ip => '176.10.168.227',
+            network_domain   => 'triatagroup.com',
+        }
     }
     else {
         fail("FAIL: Host IPv4 (${::ipaddress}) is not on our managed network!")
     }
-
+    ##
     ## Install puppet_server configuration
+    ##
     if $::ipaddress == '192.168.0.222' {
         class { puppet_server::config :
             server_fqdn => 'puppet.home.tld',
             server_ip   => '192.168.0.222',
         }
         include hieradata
+        include hosts
     }
     elsif $::ipaddress == '176.10.168.227' {
         class { puppet_server::config :
@@ -36,6 +48,7 @@ class profile::puppetize {
             server_ip   => '176.10.168.227',
         }
         include hieradata
+        include hosts
     }
 
 }
