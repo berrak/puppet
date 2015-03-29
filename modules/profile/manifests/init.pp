0a32d6b6104e443d8366187a6b21f89cd63f010e
berrak <bkronmailbox-git@yahoo.se>
2015-03-29 16:14:34 +0200

    Add gitclient to module

--- modules/profile/manifests/init.pp
+++ modules/profile/manifests/init.pp
@@ -1,7 +1,11 @@
 ##
-## Bootstrap puppet
+## Technology Profiles
 ##
 class profile {
+
+    if ! ( $::operatingsystem == 'Debian' ) {
+        fail('FAIL: This module (profile) is only for Debian based distributions! Aborting...')
+    }
 
     include profile::puppetize
 
