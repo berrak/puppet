e93cd6b3df5be47ea3301d957f9901129f18d078
berrak <bkronmailbox-git@yahoo.se>
2015-03-29 17:10:14 +0200

    Add markdown profile

---
+++ modules/profile/manifests/markdown_development.pp
@@ -0,0 +1,11 @@
+##
+##  Sample use:
+##  profile::markdown_development { 'bekr': }
+##
+define profile::markdown_development {
+
+    include development_editors
+
+    ## Add user customization
+
+}
